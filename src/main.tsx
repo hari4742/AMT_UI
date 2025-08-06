d6ff0224d463718d7c66d5189ec01a79e7bab32f
HariKiran <harikiran.kante@gmail.com>
2025-08-06 19:36:41 +0530

    Refactor main.tsx and tsconfig.json for consistency; update import statements and adjust TypeScript compiler options.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import App from './App.tsx'
-import { store } from './store/store.ts'
-import { theme } from './theme/theme.ts'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App';
+import { store } from './store/store';
+import { theme } from './theme/theme';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -19,5 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </ThemeProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
