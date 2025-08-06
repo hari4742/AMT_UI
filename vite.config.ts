8dea8b9ddd3b8e526517ddbe732e28d87e4b95a9
HariKiran <harikiran.kante@gmail.com>
2025-08-06 19:13:31 +0530

    Add GitHub Actions workflow for deploying Vite React app to GitHub Pages; update package.json for deployment scripts and dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AMT_UI/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -14,4 +15,4 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-})
+});
